6868b9885c32c8d64e264f422252294f73889e60
Roy Eugene <71348610+roy-eugene049@users.noreply.github.com>
2022-06-16 13:29:31 +0300

    Update main.h

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -9,7 +9,3 @@ char *rot13(char *);
 void print_number(int n);
 char *infinite_add(char *n1, char *n2, char *r, int size_r);
 void print_buffer(char *b, int size);
-© 2022 GitHub, Inc.
-Terms
-Privacy
-Se
