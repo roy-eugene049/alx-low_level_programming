d9d514da70509805864e2ceda69c48f1eb6ab898
Roy Eugene <71348610+roy-eugene049@users.noreply.github.com>
2022-09-09 13:27:53 +0300

    Initial Commit

--- 0x18-dynamic_libraries/0-main.c
+++ 0x18-dynamic_libraries/0-main.c
@@ -1,1 +1,13 @@
-ll
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check the code
+ *
+ * Return: Always EXIT_SUCCESS.
+ */
+int main(void)
+{
+	printf("%d\n", _strlen("My Dyn Lib"));
+	return (EXIT_SUCCESS);
+}
